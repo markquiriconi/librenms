f9f72183bb10b9198a80ed7104781aea3cbd25e6
Mark <49886918+markquiriconi@users.noreply.github.com>
2022-10-07 06:53:55 -0400

    Testing counter diffs

---
+++ includes/polling/sensors/count/broadsoft.inc.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * Cisco Broadsoft
+ * Mark Quiriconi
+*/
+
+/* BW-WebContainer::bwHttpOverallResponsesNbOfResponses Count Delta */
+if (str_contains($sensor['sensor_oid'], '.1.3.6.1.4.1.6431.1.17.1.6.1.10.1.4'))
+{
+    $sensor_value = abs($sensor['sensor_current'] - $sensor_value);
+}
+
+/* BW-WebContainer::bwApplicationResourceRequestCount Count Delta */
+if (str_contains($sensor['sensor_oid'], '.1.3.6.1.4.1.6431.1.17.4.1.1.1.3'))
+{
+    $sensor_value = abs($sensor['sensor_current'] - $sensor_value);
+}
