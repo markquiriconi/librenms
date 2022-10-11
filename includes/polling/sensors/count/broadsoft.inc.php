10124d1ad602444c213d37d9a402a7c6d0eaf8cd
Mark Quiriconi <49886918+markquiriconi@users.noreply.github.com>
2022-10-11 10:23:10 -0400

    Update broadsoft.inc.php

--- includes/polling/sensors/count/broadsoft.inc.php
+++ includes/polling/sensors/count/broadsoft.inc.php
@@ -6,13 +6,14 @@
 */
 
 /* BW-WebContainer::bwHttpOverallResponsesNbOfResponses Count Delta */
-if (str_contains($sensor['sensor_oid'], '.1.3.6.1.4.1.6431.1.17.1.6.1.10.1.4'))
+/* if (str_contains($sensor['sensor_oid'], '.1.3.6.1.4.1.6431.1.17.1.6.1.10.1.4'))
 {
     $sensor_value = abs($sensor['sensor_current'] - $sensor_value);
 }
-
+*/
 /* BW-WebContainer::bwApplicationResourceRequestCount Count Delta */
-if (str_contains($sensor['sensor_oid'], '.1.3.6.1.4.1.6431.1.17.4.1.1.1.3'))
+/*if (str_contains($sensor['sensor_oid'], '.1.3.6.1.4.1.6431.1.17.4.1.1.1.3'))
 {
     $sensor_value = abs($sensor['sensor_current'] - $sensor_value);
 }
+*/
